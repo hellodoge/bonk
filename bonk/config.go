0333e6f1fdd35435b21e7eaa440b179fdcaf77fc
waldemar <hellodoge@tuta.io>
2021-06-30 15:26:35 +0300

    add MaxRequestRetries to Config struct

--- bonk/config.go
+++ bonk/config.go
@@ -3,9 +3,10 @@ package bonk
 import "time"
 
 type Config struct {
-	PeerID         PeerID
-	Port           uint16
-	TrackerTimeout time.Duration
-	PeerTimeout    time.Duration
-	RequestCompact bool
+	PeerID            PeerID
+	Port              uint16
+	TrackerTimeout    time.Duration
+	PeerTimeout       time.Duration
+	RequestCompact    bool
+	MaxRequestRetries uint
 }
