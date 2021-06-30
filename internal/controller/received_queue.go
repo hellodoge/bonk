106507b1893c03938781cd753f38b377746f1bd3
waldemar <hellodoge@tuta.io>
2021-06-30 15:25:47 +0300

    (internal) add queue types

---
+++ internal/controller/received_queue.go
@@ -0,0 +1,5 @@
+package controller
+
+import "github.com/hellodoge/bonk/bonk"
+
+type ReceivedQueue chan bonk.Piece
---
+++ internal/controller/task_queue.go
@@ -0,0 +1,5 @@
+package controller
+
+import "github.com/hellodoge/bonk/bonk"
+
+type TaskQueue chan *bonk.PieceInfo
